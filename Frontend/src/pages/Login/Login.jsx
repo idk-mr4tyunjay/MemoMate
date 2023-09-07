a95a134531a92e143c28dc6ccf3ac3c239bd1ee8
Mruthunjay <idk.mratyunjay@gmail.com>
2023-09-07 23:00:00 +0530

    login-page

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom"
+import Navbar from "../../components/Navbar/Navbar"
+import PasswordInput from "../../components/Input/PasswordInput"
 
 function Login() {
   return (
     <>
-        <Navbar />
+        <Navbar/>
+        <div className="flex items-center justify-center mt-28">
+            <div className="w-96 border rounded bg-white px-7 py-10">
+                <form onSubmit={()=> {}}>
+                    <h4 className="text-2xl mb-7">Login</h4>
+                    <input type="text" placeholder="Email" className="input-box"/>
+
+                    <PasswordInput/>
+
+                    <button type="submit" className="btn-primary">Login</button>
+                    <p className="text-sm text-center mt-4"> Not registered yet? 
+                    <Link to="/signUp" className="font-medium text-primary underline" > Create an account </Link>
+                    </p>
+                </form>
+            </div>
+        </div>
     </>
   )
 }
