1626e751458f49f3f5cdee7f02a32f6a67844640
Mruthunjay <idk.mratyunjay@gmail.com>
2024-10-09 10:50:55 +0530

    Fix:Frontend Production build fix

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-const config = require("./config.json");
 const mongoose = require("mongoose");
 
 const connectionString = process.env.CONNECTION_STRING;
--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://localhost:8000'
+export const BASE_URL = 'https://memomate-g8w5.onrender.com'
